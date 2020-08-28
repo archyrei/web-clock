508f88a37f5fa8bb70082b11325fd85114203270
redmafia <mysteriousonliner@gmail.com>
2020-08-28 21:08:31 +0700

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,30 @@ module.exports = {
     },
     mode : "production",
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                {
-                    loader: "style-loader"
-                },
-                {
-                    loader: "css-loader"
-                }
-            ]
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
