0270f78fc4e95eadbe94e74093c3f1f2f0c80d4d
redmafia <mysteriousonliner@gmail.com>
2020-08-29 22:00:20 +0700

    menmbahkan plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -33,5 +34,12 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        // HTML Webpack Plugin
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            filename: "index.html"
+        })
+    ]
 }
